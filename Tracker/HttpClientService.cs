3a20d467055a5604c53467883f40b1b20c52e894
HighNoteZayn <114074389+HighNoteZayn@users.noreply.github.com>
2025-05-31 00:20:07 +0700

    Add files via upload
    services

---
+++ Tracker/HttpClientService.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+public class HttpClientService
+{
+    private readonly HttpClient _client;
+    private readonly string _baseUrl = "https://localhost:5001/api/task"; // Ganti port jika beda
+
+    public HttpClientService()
+    {
+        _client = new HttpClient();
+    }
+
+    public async Task<List<TaskModel>> GetTasksAsync()
+    {
+        var tasks = await _client.GetFromJsonAsync<List<TaskModel>>(_baseUrl);
+        return tasks ?? new List<TaskModel>();
+    }
+
+    public async Task<bool> CreateTaskAsync(TaskModel task)
+    {
+        var response = await _client.PostAsJsonAsync(_baseUrl, task);
+        return response.IsSuccessStatusCode;
+    }
+}
