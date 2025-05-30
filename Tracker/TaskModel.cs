560df907718582edbb056bf102ed3f9d365bd4cf
HighNoteZayn <114074389+HighNoteZayn@users.noreply.github.com>
2025-05-31 00:19:34 +0700

    Add files via upload
    model

---
+++ Tracker/TaskModel.cs
@@ -0,0 +1,7 @@
+﻿public class TaskModel
+{
+    public int Id { get; set; }
+    public string Judul { get; set; } = "";
+    public string Deskripsi { get; set; } = "";
+    public DateTime Tanggal { get; set; }
+}
